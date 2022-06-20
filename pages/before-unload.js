95695ec057d2b37777e93e7624bb5ea09f9db728
Abenezer Zeleke <azeleke@excellerentsolutions.com>
2022-06-20 16:22:04 +0300

    before unload if else for browser

--- pages/before-unload.js
+++ pages/before-unload.js
@@ -1,68 +1,66 @@
 import React, { useEffect } from 'react'
-import {useRouter} from 'next/router'
+import { useRouter } from 'next/router'
 import Link from 'next/link';
 
 const BeforeUnloadCheck = () => {
     const router = useRouter();
 
     function isSafariMobile() {
-      return (
-        navigator &&
-        /Safari/.test(navigator.userAgent) &&
-        /iPhone|iPad/.test(navigator.userAgent)
-      );
+        return (
+            navigator &&
+            /Safari/.test(navigator.userAgent) &&
+            /iPhone|iPad/.test(navigator.userAgent)
+        );
     }
-  
-    const warningText =
-      "You have unsaved changes - are you sure you wish to leave this page?";
-  
-    const handleWindowClose = (e) => {
-      e.preventDefault();
-      return (e.returnValue = warningText);
-    };
-  
-    const handleBrowseAway = () => {
-      if (window.confirm(warningText)) return;
-      router.events.emit("routeChangeError");
-      throw "routeChange aborted.";
-    };
-  
-    useEffect(() => {
-      window.addEventListener("beforeunload", handleWindowClose);
-      router.events.on("routeChangeStart", handleBrowseAway);
 
-      if(isSafariMobile) {
-        window.addEventListener('focus', function(){
-            // do stuff
-            handleBrowseAway
-          });
-     
-        window.addEventListener('blur', function(){
-            // do stuff
-            handleBrowseAway
-          });
-      }
-      return () => {
-        window.removeEventListener("beforeunload", handleWindowClose);
-        router.events.off("routeChangeStart", handleBrowseAway);
-        if(isSafariMobile){
-            window.removeEventListener('focus', function(){
-                // do stuff
-            handleBrowseAway()
-              });
-         
-            window.removeEventListener('blur', function(){
-                // do stuff
-            handleBrowseAway()
-              });
+    const warningText =
+        "You have unsaved changes - are you sure you wish to leave this page?";
+
+    const handleWindowClose = (e) => {
+        e?.preventDefault();
+        return (e.returnValue = warningText);
+    };
+
+    const handleBrowseAway = () => {
+        if (window.confirm(warningText)) return;
+        router.events.emit("routeChangeError");
+        throw "routeChange aborted.";
+    };
+
+    console.log('is safari mobile',isSafariMobile())
+
+    useEffect(() => {
+        if (isSafariMobile()) {
+            window.addEventListener('focus',handleBrowseAway);
+            window.addEventListener('blur',handleBrowseAway);
         }
-      };
+        else {
+            window.addEventListener("beforeunload", handleBrowseAway);
+            router.events.on("routeChangeStart", handleBrowseAway);
+        }
+        return () => {
+            if (isSafariMobile()) {
+                window.removeEventListener('focus',handleBrowseAway);
+                window.removeEventListener('blur', handleBrowseAway);
+            }
+            else {
+                window.removeEventListener("beforeunload", handleBrowseAway);
+                router.events.off("routeChangeStart", handleBrowseAway);
+            }
+        };
     }, []);
-  return (
-    <div> 
-        <Link href={'/'}>Home</Link>
-    </div>
-  )
+    return (
+        <div className='flex flex-col w-full min-h-[80vh] items-center justify-center' style={{
+            display:'flex',
+            flexDirection:'column',
+            width:'100%',
+            minHeight:'80vh',
+            justifyContent:'center',
+            alignItems:'center'
+          }}>
+            <Link href={'/'}>Home</Link>
+        </div>
+    )
 }
 
 export default BeforeUnloadCheck
--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,14 @@ import Link from 'next/link'
 
 export default function Home() {
   return (
-    <div>
+    <div className='flex flex-col w-full min-h-[80vh] items-center justify-center' style={{
+      display:'flex',
+      flexDirection:'column',
+      width:'100%',
+      minHeight:'80vh',
+      justifyContent:'center',
+      alignItems:'center'
+    }}>
       <Link href={'/before-unload'}>Go To Before Unload</Link>
     </div>
   )
