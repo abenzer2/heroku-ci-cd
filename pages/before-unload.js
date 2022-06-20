3c982c99504b56c064df58f2c627836c03f80b12
Abenezer Zeleke <azeleke@excellerentsolutions.com>
2022-06-20 17:10:49 +0300

    check for ios

--- pages/before-unload.js
+++ pages/before-unload.js
@@ -1,10 +1,9 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useRouter } from 'next/router'
 import Link from 'next/link';
 
 const BeforeUnloadCheck = () => {
     const router = useRouter();
-
     function isSafariMobile() {
         return (
             navigator &&
@@ -12,13 +11,19 @@ const BeforeUnloadCheck = () => {
             /iPhone|iPad/.test(navigator.userAgent)
         );
     }
+    const [isIphone, setIsIphone] = useState(null)
+
+    useEffect(() => {
+        setIsIphone(isSafariMobile())
+    }, [])
+
 
     const warningText =
         "You have unsaved changes - are you sure you wish to leave this page?";
 
     const handleWindowClose = (e) => {
         e?.preventDefault();
-        return (e.returnValue = warningText);
+        return (warningText);
     };
 
     const handleBrowseAway = () => {
@@ -28,35 +33,40 @@ const BeforeUnloadCheck = () => {
     };
 
     useEffect(() => {
-        // if (isSafariMobile()) {
-            window.addEventListener('focus',handleBrowseAway);
-            window.addEventListener('blur',handleBrowseAway);
-        // }
-        // else {
-        //     window.addEventListener("beforeunload", handleBrowseAway);
-        //     router.events.on("routeChangeStart", handleBrowseAway);
-        // }
+        if (isSafariMobile()) {
+        window.addEventListener('focus',handleBrowseAway);
+        window.addEventListener('blur',handleBrowseAway);
+        }
+        else {
+        window.addEventListener("beforeunload",handleWindowClose);
+        router.events.on("routeChangeStart", handleBrowseAway);
+        }
         return () => {
-            // if (isSafariMobile()) {
-                window.removeEventListener('focus',handleBrowseAway);
-                window.removeEventListener('blur', handleBrowseAway);
-            // }
-            // else {
-            //     window.removeEventListener("beforeunload", handleBrowseAway);
-            //     router.events.off("routeChangeStart", handleBrowseAway);
-            // }
+            if (isSafariMobile()) {
+            window.removeEventListener('focus',handleBrowseAway);
+            window.removeEventListener('blur',handleBrowseAway);
+            }
+            else {
+            window.removeEventListener("beforeunload", handleWindowClose);
+            router.events.off("routeChangeStart",handleBrowseAway);
+            }
         };
-    }, []);
+    }, [isIphone]);
     return (
         <div className='flex flex-col w-full min-h-[80vh] items-center justify-center' style={{
-            display:'flex',
-            flexDirection:'column',
-            width:'100%',
-            minHeight:'80vh',
-            justifyContent:'center',
-            alignItems:'center'
-          }}>
+            display: 'flex',
+            flexDirection: 'column',
+            width: '100%',
+            minHeight: '80vh',
+            justifyContent: 'center',
+            alignItems: 'center'
+        }}>
             <Link href={'/'}>Home</Link>
+            <button onClick={() => setUserAgent(navigator.userAgent)}>Show User Agent</button>
+            <div>
+                {/* {JSON.stringify(/iPhone/.test(navigator.userAgent))} */}
+                {JSON.stringify(isIphone)}
+            </div>
         </div>
     )
 }
